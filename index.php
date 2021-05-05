6a8649e0160f16297938aff7c2c8559670100441
1233442 <jgelias01@gmail.com>
2021-05-04 23:00:10 -0400

    canales nuevos

--- index.php
+++ index.php
@@ -146,12 +146,76 @@ if($varsession == null || $varsession = '') {
     
                 <div class="card mb-4 shadow-sm">
                     <div class="card-body">
-                        <img src="imagenes_de_los_canales/" class="card-img-top" height="200">
-                        <a href="" class="btn btn-block btn-primary">Ver</a>
+                        <img src="imagenes_de_los_canales/bonao tv.jpeg" class="card-img-top" height="200">
+                        <a href="canal8.php" class="btn btn-block btn-primary">Ver</a>
                     </div>
                 </div>
 
             </div>
+
+            <div class="card-deck mb-3 text-center" id="contenido">
+            
+            <div class="card mb-4 shadow-sm">
+                <div class="card-body">
+                    <img src="imagenes_de_los_canales/teleunion.jpeg" class="card-img-top" height="200">
+                    <a href="canal16.php" class="btn btn-block btn-primary">Ver</a>
+                </div>
+            </div>
+
+            <div class="card mb-4 shadow-sm">
+                <div class="card-body">
+                    <img src="imagenes_de_los_canales/peravia_vision.jpeg" class="card-img-top" height="200">
+                    <a href="canal12.php" class="btn btn-block btn-primary">Ver</a>
+                </div>
+            </div>
+
+            <div class="card mb-4 shadow-sm">
+                <div class="card-body">
+                    <img src="imagenes_de_los_canales/COLOR-VISION.jpg" class="card-img-top" height="200">
+                    <a href="colorvision.php" class="btn btn-block btn-primary">Ver</a>
+                </div>
+            </div>
+
+            <div class="card mb-4 shadow-sm">
+                <div class="card-body">
+                    <img src="imagenes_de_los_canales/Canal-4.png" class="card-img-top" height="200">
+                    <a href="canal4.php" class="btn btn-block btn-primary">Ver</a>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="card-deck mb-3 text-center" id="contenido">
+        
+            <div class="card mb-4 shadow-sm">
+                <div class="card-body">
+                    <img src="imagenes_de_los_canales/teleuniverso.jpg" class="card-img-top" height="200">
+                    <a href="teleuniverso.php" class="btn btn-block btn-primary">Ver</a>
+                </div>
+            </div>
+
+            <div class="card mb-4 shadow-sm">
+                <div class="card-body">
+                    <img src="imagenes_de_los_canales/" class="card-img-top" height="200">
+                    <a href="" class="btn btn-block btn-primary">Ver</a>
+                </div>
+            </div>
+
+            <div class="card mb-4 shadow-sm">
+                <div class="card-body">
+                    <img src="imagenes_de_los_canales/" class="card-img-top" height="200">
+                    <a href=".html" class="btn btn-block btn-primary">Ver</a>
+                </div>
+            </div>
+
+            <div class="card mb-4 shadow-sm">
+                <div class="card-body">
+                    <img src="imagenes_de_los_canales/" class="card-img-top" height="200">
+                    <a href="" class="btn btn-block btn-primary">Ver</a>
+                </div>
+            </div>
+
+        </div>
 
         </div>
 
